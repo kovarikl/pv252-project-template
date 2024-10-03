191aebbf883df2bd5e480c6d1c0bd59168d1a542
Lukáš Kovařík <kovariklukas@outlook.com>
2024-10-03 19:10:19 +0200

    Make test-ui job failing

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -2,6 +2,6 @@ import { expect } from "@playwright/test";
 import { test } from "./coverage_wrapper";
 
 test("find-watman", async ({ page }) => {  
-  await page.goto("/");
+  await page.goto("/s");
   await expect(page.getByAltText("This is watman")).toBeInViewport();
 });
